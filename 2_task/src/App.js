2c08caba989e974ca6c040b1f60cac0cdcf26525
Anh Le <maianh_tom@yahoo.com.vn>
2021-02-02 00:42:58 +0200

    Animal cards

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Animal from './Components/Animals/Animal';
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="cards">
+      <Animal name="Fox" img="https://source.unsplash.com/xUUZcpQlqpM"/>
+      <Animal name="Bird" img="https://source.unsplash.com/RLLR0oRz16Y"/>
+      <Animal name="Fish" img="https://source.unsplash.com/rEM3cK8F1pk"/>
+     
     </div>
   );
 };
